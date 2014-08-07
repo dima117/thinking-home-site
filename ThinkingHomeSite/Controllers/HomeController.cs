04c27de4d48acf811799ba9d33d9b65f9310b316
dima117 <dima117a@gmail.com>
2014-08-07 19:52:43 +0400

    - перенесены страница и верстка со старой версии сайта;

---
+++ ThinkingHomeSite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace ThinkingHomeSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult GettingStarted()
+		{
+			return View();
+		}
+
+		public ActionResult Scripts()
+		{
+			return View();
+		}
+
+		public ActionResult Plugins()
+	    {
+			return View();
+	    }
+
+	    public ActionResult UiForPlugins()
+	    {
+			return View();
+	    }
+    }
+}
