c8989a2593ff2eb2070b17934e5e8699baa1a01e
dima117 <dima117a@gmail.com>
2014-10-01 10:26:07 +0400

    - роутинг и раздел о начале работы;

--- ThinkingHomeSite/App_Start/RouteConfig.cs
+++ ThinkingHomeSite/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ThinkingHomeSite
@@ -12,6 +8,30 @@ namespace ThinkingHomeSite
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: "getting-started",
+				url: "getting-started",
+				defaults: new { controller = "Home", action = "GettingStarted" }
+			);
+
+			routes.MapRoute(
+				name: "home-scripts",
+				url: "home-scripts",
+				defaults: new { controller = "Home", action = "Scripts" }
+			);
+
+			routes.MapRoute(
+				name: "plugins",
+				url: "plugins",
+				defaults: new { controller = "Home", action = "Plugins" }
+			);
+
+			routes.MapRoute(
+				name: "ui-for-plugins", 
+				url: "ui-for-plugins",
+				defaults: new { controller = "Home", action = "UiForPlugins" }
+			);
 
 			routes.MapRoute(
 				name: "Default",
